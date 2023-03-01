e47bde85038ab299ee9c9080719e45e9858eddb2
subhayanmondal <sooobhayan@gmail.com>
2023-03-01 15:23:39 +0530

    correct sip

--- Functions/sip_calculator.R
+++ Functions/sip_calculator.R
@@ -9,13 +9,3 @@ sip_calculator <- function(sip_amount, tenure_in_years, expected_return){
     amount <- sapply(time_seq, function(x){sip_amount*(1 + (r_monthly/100))^x})
     sum(amount)
 }
-
-# sip_calculator(sip_amount = 50e3, tenure_in_years = 10, expected_return = 12)
-# 
-sip_amount <- c(5e3, 10e3)
-tenure_in_months <- 20*12
-time_seq <- 1:tenure_in_months
-r_monthly <- c(16/12, 0/12)
-
-amount <- lapply(time_seq, function(x){sip_amount*(1 + (r_monthly/100)^x)})
-
--- Scripts/compare_sip_returns.R
+++ Scripts/compare_sip_returns.R
@@ -13,9 +13,9 @@ lapply(modules, library, character.only = TRUE)
 
 source("./Functions/sip_calculator.R")
 
-sip_amount = 3.7e3
-tenure_in_years = 25
-expected_return = seq(0, 16, 4)
+sip_amount = 3.55e3
+tenure_in_years = 23
+expected_return = c(0, seq(1, 15, 2), 6)
 final_corpus <- sip_calculator(sip_amount, tenure_in_years, expected_return)
 
 data <- data.frame(sip_amount, 
